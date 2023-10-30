b45207d890dc1053337edd05feab21c24ffe474a
Cade Wilson <cade25wilson@gmail.com>
2023-10-29 18:59:27 -0600

    productlayoutupdate

--- ClientApp/suppsclient/src/components/ProductLayout.vue
+++ ClientApp/suppsclient/src/components/ProductLayout.vue
@@ -18,39 +18,40 @@ export default{
 <template>
     
     <div class="border rounded p-3 h-100">
-          <ProductImage :image="product.image" :url="product.url"/>
+      <!-- {{ product }} -->
+          <ProductImage :image="product.supplement.image" :url="product.url"/>
           <div class="product-name h-auto">
-            <h5>{{product.name}}</h5>
+            <h5>{{product.supplement.name}}</h5>
             <div class="row h-auto">
               <div class="col-3">
                 <div class="product-price">
                   <p> 
-                    <span class="text-danger"><del>${{ product.originalPrice }}</del></span>
-                    ${{ product.discountPrice }}
+                    <span class="text-danger"><del>${{ product.supplement.originalPrice }}</del></span>
+                    ${{ product.supplement.discountPrice }}
                   </p>
                 </div>
               </div>
               <div class="col-9">
-                <div class="product-brand text-end">
-                  <a href="/" class="text-black">
-                    <p class="mb-0" v-if="product.brand">{{ product.brand }}</p>
-                  </a>
+                <div class="product-brand text-end" v-if="product.brand">
+                  <router-link :to="`/brand/`+product.brand.brandUrl" class="text-black">
+                    <p class="mb-0" >{{ product.brand.brandName }}</p>
+                  </router-link>
                 </div>
               </div>
             </div>
             <div class="row">
               <div class="col-3">
-                <p>${{product.discount}}🔻</p>
+                <p>${{product.supplement.discount}}🔻</p>
               </div>
               <div class="col-9 text-end" v-if="product.advertiser">
                 <a href="/" class="text-black">
-                  <p>{{ product.advertiser }}</p>
+                  <p>{{ product.supplement.advertiser }}</p>
                 </a>
               </div>
             </div>
             <div class="row">
               <div class="text-center">
-                <a :href="product.url" class="btn btn-primary w-75" target="_blank">View Product</a>
+                <a :href="product.supplement.url" class="btn btn-primary w-75" target="_blank">View Product</a>
               </div>
             </div>
           </div>
